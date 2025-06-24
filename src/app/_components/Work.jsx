616c370af1170a79240dd6736440c98d6e158cc0
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-06-24 23:37:14 +0530

    completed my work section

--- src/app/_components/Work.jsx
+++ src/app/_components/Work.jsx
@@ -17,16 +17,20 @@ const Work = () => {
             <div key={index} style={{ backgroundImage: `url(${project.bgImage})` }} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative group'>
               <div className='absolute bg-white w-10/12 rounded-md left-1/2 -translate-x-1/2 group-hover:bottom-7 bottom-5 duration-500 flex items-center justify-between px-5 py-3'>
                 <div>
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
+                    <h2 className='font-semibold'>{project.title}</h2>
+                    <p className='text-sm text-gray-700'>{project.description}</p>
                 </div>
-                <div>
-                    <Image src={assets.send_icon} alt='send' className='w-5'/>
+                <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0px_#000] group-hover:bg-lime-300 cursor-pointer transition'>
+                    <Image src={assets.send_icon} alt='send' className='w-3'/>
                 </div>
              </div>
             </div>
         ))}
       </div>
+
+      <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
+       Show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4'/>
+      </a>
 
     </div>
   )
