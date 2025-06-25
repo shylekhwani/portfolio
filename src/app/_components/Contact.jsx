010137bf839b9368b30fcde8f599f682efc5d2a3
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-06-25 11:58:19 +0530

    bug fix and improved contact setion

--- src/app/_components/Contact.jsx
+++ src/app/_components/Contact.jsx
@@ -1,8 +1,36 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
+import { useState } from 'react'
 import { assets } from '../../../assets/assets'
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "7dcf8132-bc6e-42d8-b225-96c78276ae43");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+    };
+
   return (
     <div id='Contact' className='w-full px-6 sm:px-[8%] lg:px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-[length:90%_auto] bg-center'>
       <h4 className='text-center mb-2 text-base sm:text-lg font-Ovo'>Connect With Me</h4>
@@ -12,12 +40,12 @@ const Contact = () => {
        I'd love to hear from you! Whether you have a question, want to discuss a project, or just want to say hello, feel free to reach out. please use the contact form below or connect with me on social media. Let's create something amazing together!
       </p>
 
-      <form className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10 mb-8'>
-            <input type='text' placeholder='Enter your name' required className='p-3 outline-none border border-gray-400 rounded-md bg-white' />
-            <input type='email' placeholder='Enter your email' required className='p-3 outline-none border border-gray-400 rounded-md bg-white' />
+            <input type='text' placeholder='Enter your name' required className='p-3 outline-none border border-gray-400 rounded-md bg-white' name='name'/>
+            <input type='email' placeholder='Enter your email' required className='p-3 outline-none border border-gray-400 rounded-md bg-white' name='email' />
         </div>
-        <textarea rows={6} placeholder='Enter your message' required className='w-full p-4 outline-none border border-gray-400 rounded-md bg-white mb-6'></textarea>
+        <textarea rows={6} placeholder='Enter your message' required className='w-full p-4 outline-none border border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
 
         <div className="flex justify-center">
             <button
@@ -28,6 +56,7 @@ const Contact = () => {
             <Image src={assets.right_arrow_white} alt='arrow' className='w-4' />
             </button>
         </div>
+        <p className='mt-4'>{result}</p>
       </form>
     </div>
   )
